26c8332667bad0027d3ce416a092dba904547058
Mike Smith <smith.mike@mac.com>
2019-09-13 21:11:16 -0400

    created product and store js files for models.

---
+++ models/products.js
@@ -0,0 +1,19 @@
+module.exports = function(sequelize, DataTypes) {
+  var Product = sequelize.define("Product", {
+    productName: DataTypes.STRING,
+    upc: DataTypes.INTERGER,
+    image: DataTypes.STRING
+  });
+
+  Product.associate = function(models) {
+    // Associating Products with Stores
+    Product.belongsToMany(models.Store, {
+      through: "Price",
+      as: "products",
+      foreignKey: "productId",
+      otherKey: "storeId"
+    });
+  };
+
+  return Product;
+};
