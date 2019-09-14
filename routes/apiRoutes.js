322c4d2e54830d5ff07b11c4075e79f1a21c34f7
Aaron Lanier <alanier.william@gmail.com>
2019-09-14 10:17:41 -0400

    finished api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,65 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get route for user view
+  app.get("/api/price/:productId", function(req, res) {
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.json(dbExamples);
+    // });
+    db.Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Product]
+    }).then(function(dbProduct) {
+      res.json(dbProduct);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // get route for manager view
+  app.get("/api/products", function(req, res) {
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.json(dbExamples);
+    // });
+    res.json([
+      { id: 1,
+        productName: "banana",
+        upc: 0000000094011,
+        image: "https://www.kroger.com/product/images/medium/front/0000000094011"
+      },
+      { id: 2,
+        productName: "apple",
+        upc: 0000000094012,
+        image: "https://fillmurray.com/200/300"
+      },
+      { id: 3,
+        productName: "pear",
+        upc: 0000000094013,
+        image: "https://fillmurray.com/200/400"
+      },
+      { id: 4,
+        productName: "coconut",
+        upc: 0000000094014,
+        image: "https://fillmurray.com/200/500"
+      }
+    ]);
+  });
+
+  // POST route for manager view
+  app.post("/api/products", function(req, res) {
+    console.log(req.body);
+    // create takes an argument of an object describing the item we want to
+    // insert into our table. In this case we just we pass in an object with a text
+    // and complete property (req.body)
+    db.Product.create({
+      product_name: req.body.product_name,
+      upc: req.body.upc,
+      price: req.body.price
+    }).then(function(dbProduct) {
+      // We have access to the new todo as an argument inside of the callback function
+      res.json(dbProduct);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
-  });
+
 };
