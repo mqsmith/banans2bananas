39fb733408e2b2f9fcbe439aad8cf3f5478b7174
Aaron Lanier <alanier.william@gmail.com>
2019-09-16 13:50:09 -0400

    have html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,27 +3,16 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "index.html"));
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/adminView", function(req, res) {
+    res.sendFile(path.join(__dirname, "manager_view.html"));
   });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.render("404");
+    res.sendFile(path.join(__dirname, "page404.html"));
   });
 };
