f9d703f64ffd08763245a03648c738d3100e6f1e
Mike Smith <smith.mike@mac.com>
2019-09-17 12:15:03 -0400

    Converted SQL to Sequelize for the price query.

--- models/products.js
+++ models/products.js
@@ -5,13 +5,25 @@ module.exports = function(sequelize, DataTypes) {
     image: DataTypes.STRING
   });
 
-  Product.associate = function(models) {
-    // Associating Products with Stores
-    Product.belongsToMany(models.Store, {
-      through: "Price",
-      as: "products",
-      foreignKey: "productId",
-      otherKey: "storeId"
+  // Product.associate = function(models) {
+  //   // Associating Products with Stores
+  //   Product.belongsToMany(models.Store, {
+  //     through: "Price",
+  //     as: "products",
+  //     foreignKey: "productId",
+  //     otherKey: "storeId"
+  //   });
+  // };
+
+  Product.seed = function() {
+    console.log("called seed product");
+    Product.create({
+      productName: "bananas",
+      upc: "0000000094011"
+    });
+    Product.create({
+      productName: "toilet paper",
+      upc: "0000000094015"
     });
   };
 
